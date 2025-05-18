7d7c09a42cc1ed56df3a76c0e7292842735c487f
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-05-18 16:24:28 -0300

    fix: aws server location

--- terraform/state.tf
+++ terraform/state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "aws-bootcamp-cm-42"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
     use_lockfile = true
   }
 }
