27183f13583858369c3f3a9aef4664654df8a227
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-05-20 22:35:39 -0300

    fix: remove single quotes

--- terraform/aws_vpc.tf
+++ terraform/aws_vpc.tf
@@ -36,7 +36,7 @@ resource "aws_route_table" "public_a" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = '0.0.0.0/0'
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
 }
@@ -49,7 +49,7 @@ resource "aws_route_table" "public_b" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = '0.0.0.0/0'
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
 }
