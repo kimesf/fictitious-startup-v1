d6406060dc523315acabf6748299dea5d7f749b3
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-07 17:38:27 -0300

    fix: dont rename sg

--- terraform/database.tf
+++ terraform/database.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "zone_a_private" {
     from_port = 5432
     to_port = 5432
     protocol = "tcp"
-    security_groups = [aws_security_group.zone_a_public.id]
+    security_groups = [aws_security_group.zone_a.id]
   }
 }
 
