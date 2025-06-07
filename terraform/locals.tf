294b4c304c07220f691f07de0dc8ae8693d5535c
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-07 20:20:15 -0300

    fix: terraform block is static

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,4 @@
 locals {
-  region = "us-east-2"
   region_a = "us-east-2a"
   region_b = "us-east-2b"
 }
--- terraform/state.tf
+++ terraform/state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "aws-bootcamp-cm-42"
     key    = "terraform.tfstate"
-    region = locals.region
+    region = "us-east-2"
     use_lockfile = true
   }
 }
