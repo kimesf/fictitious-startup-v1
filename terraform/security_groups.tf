a2ee8cde78f99c9298d9ee2b467779d34e3b28bc
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-07 20:28:02 -0300

    fix: remove circular dependency

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,25 +1,17 @@
-
 resource "aws_security_group" "zone_a" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port = 5432
-    to_port = 5432
-    protocol = "tcp"
-    security_groups = [aws_security_group.zone_a_private.id]
-  }
-
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -27,13 +19,24 @@ resource "aws_security_group" "zone_a" {
 resource "aws_security_group" "zone_a_private" {
   name = "zone-a-private-sg"
   vpc_id = aws_vpc.main.id
-
-  ingress {
-    description = "Allow inbound traffic from server"
-    from_port = 5432
-    to_port = 5432
-    protocol = "tcp"
-    security_groups = [aws_security_group.zone_a.id]
-  }
 }
 
+resource "aws_security_group_rule" "zone_a_to_zone_a_private" {
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  security_group_id = aws_security_group.zone_a_private.id
+  source_security_group_id = aws_security_group.zone_a.id
+  description       = "Allow inbound traffic from zone_a"
+}
+
+resource "aws_security_group_rule" "zone_a_private_to_zone_a" {
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  security_group_id = aws_security_group.zone_a.id
+  source_security_group_id = aws_security_group.zone_a_private.id
+  description       = "Allow inbound traffic from zone_a_private"
+}
