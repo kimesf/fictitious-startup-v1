c976d41f3d246d834e9b8ea84a85399f0113121f
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-07 17:31:42 -0300

    Revert "fix: sg dependance"
    This reverts commit 45e8c8df249eaf4823d0850248b672720f4529c2.

--- terraform/aws_instance.tf
+++ terraform/aws_instance.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "app_a" {
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.zone_a_public.id]
   iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
-  depends_on = [aws_security_group.zone_a_public]
 }
 
 data "aws_ami" "ubuntu" {
