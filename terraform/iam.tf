0a0ccdfa058f2f2d5151134d67033ada0c5a3204
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-07 21:20:25 -0300

    fix: remove unnecessary

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -16,6 +16,11 @@ resource "aws_iam_role_policy_attachment" "ssm_core" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "ec2_ssm_instance_profile"
+  role = aws_iam_role.ssm_role.name
+}
+
 data "aws_iam_policy_document" "dms_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -27,37 +32,17 @@ data "aws_iam_policy_document" "dms_assume_role" {
   }
 }
 
-resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = "ec2_ssm_instance_profile"
-  role = aws_iam_role.ssm_role.name
-}
-
 resource "aws_iam_role" "dms_access_for_endpoint" {
-  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
   name               = "dms_access_for_endpoint"
-}
-
-resource "aws_iam_role_policy_attachment" "dms_access_for_endpoint_AmazonDMSRedshiftS3Role" {
-  policy_arn = "arn:aws:iam::aws:policy/service_role/AmazonDMSRedshiftS3Role"
-  role       = aws_iam_role.dms_access_for_endpoint.name
-}
-
-resource "aws_iam_role" "dms_cloudwatch_logs_role" {
   assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
-  name               = "dms-cloudwatch-logs-role"
-}
-
-resource "aws_iam_role_policy_attachment" "dms_cloudwatch_logs_role_AmazonDMSCloudWatchLogsRole" {
-  policy_arn = "arn:aws:iam::aws:policy/service_role/AmazonDMSCloudWatchLogsRole"
-  role       = aws_iam_role.dms_cloudwatch_logs_role.name
 }
 
 resource "aws_iam_role" "dms_vpc_role" {
-  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
   name               = "dms-vpc-role"
+  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "dms_vpc_role_AmazonDMSVPCManagementRole" {
+resource "aws_iam_role_policy_attachment" "dms_vpc_role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
   role       = aws_iam_role.dms_vpc_role.name
 }
