a8a81fe285d4b1ce5d971562aeb62bf7c3673190
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-07 22:25:14 -0300

    fix: only use group rules

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,24 +1,30 @@
 resource "aws_security_group" "zone_a" {
   vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group" "zone_a_private" {
   name = "zone-a-private-sg"
   vpc_id = aws_vpc.main.id
+}
+
+resource "aws_security_group_rule" "zone_a_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.zone_a.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow inbound HTTP from anywhere"
+}
+
+resource "aws_security_group_rule" "zone_a_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.zone_a.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow all outbound traffic"
 }
 
 resource "aws_security_group_rule" "zone_a_to_zone_a_private" {
