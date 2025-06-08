0f514a623815dda33a7ccb3d86d27107991be1cf
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-08 00:10:58 -0300

    fix: reference to renamed

--- terraform/aws_instance.tf
+++ terraform/aws_instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "app_a" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public_a.id
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.zone_a.id]
+  vpc_security_group_ids = [aws_security_group.zone_a_public.id]
   iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
   tags = {
