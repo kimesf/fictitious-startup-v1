7e96a5b148ec24f2ef0f7c170b8981cbfd1f802d
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-17 00:08:54 -0300

    refactor

--- terraform/envs.tf
+++ terraform/envs.tf
@@ -11,15 +11,30 @@ variable "db_password" {
 }
 
 locals {
-  ssm_parameters = {
-    secret_key   = "/cloudtalents/startup/secret_key"
-    db_user      = "/cloudtalents/startup/db_user"
-    db_password  = "/cloudtalents/startup/db_password"
+  secure_ssm_parameters = {
+    secret_key  = "/cloudtalents/startup/secret_key"
+    db_user     = "/cloudtalents/startup/db_user"
+    db_password = "/cloudtalents/startup/db_password"
+  }
+
+  dynamic_ssm_parameters = {
+    database_endpoint = {
+      name  = "/cloudtalents/startup/database_endpoint"
+      value = aws_db_instance.mvp_db_instance.endpoint
+    }
+    image_storage_bucket_name = {
+      name  = "/cloudtalents/startup/image_storage_bucket_name"
+      value = aws_s3_bucket.assets.bucket
+    }
+    image_storage_cloudfront_domain = {
+      name  = "/cloudtalents/startup/image_storage_cloudfront_domain"
+      value = aws_cloudfront_distribution.assets_cdn.domain_name
+    }
   }
 }
 
-resource "aws_ssm_parameter" "webserver_secrets" {
-  for_each = local.ssm_parameters
+resource "aws_ssm_parameter" "secure_parameters" {
+  for_each = local.secure_ssm_parameters
 
   name  = each.value
   type  = "SecureString"
@@ -30,21 +45,10 @@ resource "aws_ssm_parameter" "webserver_secrets" {
   }
 }
 
-resource "aws_ssm_parameter" "database_endpoint" {
-  name  = "/cloudtalents/startup/database_endpoint"
-  type  = "String"
-  value = aws_db_instance.mvp_db_instance.endpoint
-}
+resource "aws_ssm_parameter" "dynamic_parameters" {
+  for_each = local.dynamic_ssm_parameters
 
-resource "aws_ssm_parameter" "image_storage_bucket_name" {
-  name  = "/cloudtalents/startup/image_storage_bucket_name"
+  name  = each.value.name
   type  = "String"
-  value = aws_s3_bucket.assets.bucket
+  value = each.value.value
 }
-
-resource "aws_ssm_parameter" "image_storage_cloudfront_domain" {
-  name  = "/cloudtalents/startup/image_storage_cloudfront_domain"
-  type  = "String"
-  value = aws_cloudfront_distribution.assets_cdn.domain_name
-}
-
