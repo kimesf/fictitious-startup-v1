81afb5b7a72fb11b764fa8871c6c07daeaeebed0
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-17 23:04:06 -0300

    feat: use eip on server_name nginx

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ touch $APP_DIR/tmp/nginx_config
 cat <<EOF | sudo tee $APP_DIR/tmp/nginx_config > /dev/null
 server {
     listen 80;
-    server_name 18.190.217.33;
+    server_name 3.18.63.150;
 
     location = /favicon.ico { access_log off; log_not_found off; }
 
