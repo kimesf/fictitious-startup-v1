4b6cb9aafe8d824abe09efb67dd59947c60590d0
Kim Emmanuel <kim.emmanuel@gmail.com>
2025-06-17 23:01:20 -0300

    feat: setup elastic ip

--- terraform/aws_instance.tf
+++ terraform/aws_instance.tf
@@ -11,6 +11,14 @@ resource "aws_instance" "app_a" {
   }
 }
 
+resource "aws_eip" "app_a_ip" {
+  domain = "vpc"
+
+  instance                  = aws_instance.app_a.id
+  associate_with_private_ip = aws_instance.app_a.private_ip
+  depends_on                = [aws_internet_gateway.main]
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["self"]
