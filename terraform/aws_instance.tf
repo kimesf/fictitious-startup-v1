0ee811934c3dda91eedb9f15a78376e863367170
Kim Emmanuel <41296331+kimesf@users.noreply.github.com>
2025-07-13 18:47:34 -0300

    feat: adds autoscaling + remove dms (#24)
    * feat: autoscaling
    * feat: replace instance
    * feat: remove single instance references
    * feat: remove dms sgs

--- terraform/aws_instance.tf
+++ terraform/aws_instance.tf
@@ -1,13 +1,50 @@
-resource "aws_instance" "app_a" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public_a.id
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.zone_a_public.id]
-  iam_instance_profile        = aws_iam_instance_profile.ssm_instance_profile.name
+resource "aws_launch_template" "app" {
+  name_prefix   = "app-a-"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
-  tags = {
-    Name = "app-a"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ssm_instance_profile.name
+  }
+
+  vpc_security_group_ids = [aws_security_group.zone_a_public.id]
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.zone_a_public.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "app-a"
+    }
+  }
+
+  monitoring {
+    enabled = true
+  }
+}
+
+resource "aws_autoscaling_group" "app" {
+  desired_capacity     = 1
+  min_size             = 1
+  max_size             = 5
+  vpc_zone_identifier  = [aws_subnet.public_a.id]
+
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "cloudtalents-instance"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
