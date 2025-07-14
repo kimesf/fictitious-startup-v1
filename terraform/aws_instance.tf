91fa1bb714985fc088a1284f3b329ff2d43c1895
Kim Emmanuel <41296331+kimesf@users.noreply.github.com>
2025-07-13 21:19:25 -0300

    feat: load balancer (#26)

--- terraform/aws_instance.tf
+++ terraform/aws_instance.tf
@@ -29,6 +29,7 @@ resource "aws_autoscaling_group" "app" {
   min_size            = 1
   max_size            = 5
   vpc_zone_identifier = [aws_subnet.public_a.id]
+  target_group_arns   = [aws_lb_target_group.app.arn]
 
   launch_template {
     id      = aws_launch_template.app.id
@@ -68,5 +69,43 @@ data "aws_ami" "ubuntu" {
   filter {
     name   = "name"
     values = ["cloudtalents-startup-${var.release_version}"]
+  }
+}
+
+resource "aws_lb" "app" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.zone_a_public.id]
+  subnets            = [aws_subnet.public_a.id]
+}
+
+resource "aws_lb_target_group" "app" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  target_type                   = "instance"
+  load_balancing_algorithm_type = "least_outstanding_requests"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 4
+    timeout             = 15
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
   }
 }
